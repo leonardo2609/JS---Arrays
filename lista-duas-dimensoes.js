bdb22d4546618a0435fbe1f8df5a59570259fe5d
Leojogos dionizio <dioniziosleonardo@gmail.com>
2023-03-06 11:43:36 -0300

    Curso de Arrays do JS

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,13 @@
+const nome1 = ["Ana Paula", "Leonardo", "Lyvia", "Marcelo"]
+
+const notas = [10, 9, 8, 7]
+
+const generos = ["A aluna", "O aluno", "A aluna", "O aluno"]
+
+const final = [nome1, notas, generos]
+
+
+console.log(`${final[2][0]} ${final[0][0]} tirou na prova de Matemática ${final[1][0]}.`)
+console.log(`${final[2][1]} ${final[0][1]} tirou na prova de Matemática ${final[1][1]}.`)
+console.log(`${final[2][2]} ${final[0][2]} tirou na prova de Matemática ${final[1][2]}.`)
+console.log(`${final[2][3]} ${final[0][3]} tirou na prova de Matemática ${final[1][3]}.`)
