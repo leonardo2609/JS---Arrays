b115ac65656eacce55e6b0eeb9d6ff76297146e0
Leojogos dionizio <dioniziosleonardo@gmail.com>
2023-03-06 10:36:59 -0300

    Curso de Arrays do JS

---
+++ jantar.js
@@ -0,0 +1,14 @@
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+console.log(jantarDeHoje)
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+console.log(jantarDeHoje)
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
---
+++ slice.js
@@ -0,0 +1,13 @@
+const alunos = ['Miguel', 'Helena', 'Heitor', 'Bernardo', 'Arthur', 'Maria Alice', 'Gael',
+    'Laura', 'Davi', 'Alice', 'Gabriel', 'Cecília', 'Pedro', 'Théo', 'Henrique', 'Liz', 'Ravi',
+    'Antonella', 'Valentina', 'João Miguel', 'Sophia', 'Lucas', 'Júlia', 'Noah', 'Manuela',
+    'Maria Eduarda', 'Maria Clara', 'Luisa', 'Lorenzo', 'Matheus', 'Maite', 'Isaac', 'Agatha',
+    'Samuel', 'Lara', 'Beatriz', 'Benício', 'Luiza', 'Isis', 'João Lucas', 'Esther',
+    'Maria Cecília', 'João Pedro', 'Antônio', 'Maria Luiza', 'Isabela', 'Nicolas', 'Elisa']
+
+const sala1 = alunos.slice(0, alunos.length / 2)
+const sala2 = alunos.slice(alunos.length / 2)
+
+console.log(sala1)
+console.log("=======================================")
+console.log(sala2)
