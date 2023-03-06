a73aea1727f018917401cc36c587e2d958cb5d0a
Leojogos dionizio <dioniziosleonardo@gmail.com>
2023-03-06 10:58:56 -0300

    Curso de Arrays do Js

---
+++ splice.js
@@ -0,0 +1,7 @@
+// add "Ivana" e retirar "Leonardo"
+
+const nome = ["Leonardo", "Lyvia", "Ana Paula", "Marcelo"]
+
+nome.splice(0, 1, "Ivana") // Ele trocou o "Leonardo" pela "Ivana"
+
+console.log(nome)
