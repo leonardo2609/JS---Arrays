9ccb856d47ed86bc901150b42caca88cf3a285de
Leojogos dionizio <dioniziosleonardo@gmail.com>
2023-03-07 10:38:19 -0300

    Curso de Arrays do JS

---
+++ for.js
@@ -0,0 +1,9 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+// Primeira expressão: é executada apenas uma vez
+// Segunda expressão: condição de execução
+// Terceira expressão: é executada sempre ao final do bloco
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+}
